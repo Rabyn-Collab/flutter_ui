b3078430e3cf60c56efef79a1cd20869f4dae93d
RabynCollab <rabyn900@gmail.com>
2022-01-17 08:59:54 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,18 @@ import 'package:flutter_ui/screens/home_screen.dart';
 
 
 void main (){
+
   runApp(Home());
 }
 
 
 class Home extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+       home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,36 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/models/person.dart';
 
 
 
 
 class HomeScreen extends StatelessWidget {
+
+ final titleController = TextEditingController();
+ final passwordController = TextEditingController();
+final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Messages'),
-        centerTitle: true,
-        backgroundColor: Colors.purple,
-      ),
-        body: Column(
-          children: persons.map((e) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: ListTile(
-            leading: CircleAvatar(
-                radius: 30,
-                backgroundImage: NetworkImage(e.imageUrl),
-            ),
-                  title: Text(e.userName,style: TextStyle(fontWeight: FontWeight.w500, color: e.color),),
-                  subtitle: Text(e.message),
-                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+      body: Form(
+        key: _form,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(height: 150,),
+                   Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYXNeW05OGMu4dgLwvR94x_kBUrj-ZGyIVKtF8EjQkwtF4gnLKWR_LO6puDAsj529vXwc&usqp=CAU'),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: titleController,
+                validator: (val){
+                  if(val!.isEmpty || !val.contains('@')){
+                    return 'please provide email';
+                  }else{
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20),
+                 hintText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40)
+                  )
                 ),
               ),
-            );
-          }).toList(),
-        )
+              SizedBox(height: 20,),
+              TextFormField(
+                validator: (val){
+                  if(val!.isEmpty){
+                    return 'please provide password';
+                  }else{
+                    return null;
+                  }
+                },
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40)
+                    ),
+                    hintText: 'password'
+                ),
+              ),
+                SizedBox(height: 40,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    minimumSize: Size(420, 45)
+                  ),
+                    onPressed: (){
+                _form.currentState!.save();
+                if(_form.currentState!.validate()){
+                  print(titleController.text.trim());
+                  print(passwordController.text.trim());
+
+                }
+
+                    },
+                    child: Text('Sign In')
+                ),
+                SizedBox(height: 35,),
+                Text('Forgot password', style: TextStyle(fontSize: 17, color: Colors.blueGrey, wordSpacing: 10),)
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
