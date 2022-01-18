18b10a7f71a6d509eac6974d443968f4b80018f7
RabynCollab <rabyn900@gmail.com>
2022-01-18 15:20:35 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/screens/login_screen.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+void main() => runApp(Home());
+
+
+
+class Home extends StatelessWidget {
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+        home: LoginScreen()
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,111 +1,75 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
 
-class _LoginScreenState extends State<LoginScreen> {
+
+
+class LoginScreen extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: <Widget>[
-              Image(
-                height: MediaQuery.of(context).size.height / 2.5,
-                width: double.infinity,
-                image: AssetImage('assets/images/login_background.jpg'),
-                fit: BoxFit.cover,
-              ),
-              Text(
-                'FRENZY',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 34.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 10.0,
-                ),
-              ),
-              SizedBox(height: 10.0),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Username',
-                    prefixIcon: Icon(
-                      Icons.account_box,
-                      size: 30.0,
+        body: SingleChildScrollView(
+          child: Container(
+            height:   MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+             Image.asset('assets/images/login_background.jpg', fit: BoxFit.cover,),
+              SizedBox(height: 20,),
+                Text('FRENZY', style: TextStyle(fontSize: 35, color: Colors.blue, letterSpacing: 5, fontWeight: FontWeight.bold),),
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'username',
+                      prefixIcon: Icon(Icons.account_box),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Password',
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      size: 30.0,
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                    obscureText:  true,
+                    decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'password',
+                        prefixIcon: Icon(Icons.account_box)
                     ),
                   ),
-                  obscureText: true,
                 ),
-              ),
-              SizedBox(height: 40.0),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 60.0),
-                alignment: Alignment.center,
-                height: 45.0,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 1.5,
+                SizedBox(height: 25,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(320, 40),
                   ),
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: GestureDetector(
-                    onTap: () {},
+                    onPressed: (){
+
+                    }, child:Text('Login', style: TextStyle(fontSize: 20),) ) ,
+
+                SizedBox(height: 25,),
+
+                Expanded(
                     child: Container(
-                      alignment: Alignment.center,
-                      color: Theme.of(context).primaryColor,
-                      height: 80.0,
-                      child: Text(
-                        'Don\'t have an account? Sign up',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                      alignment: FractionalOffset.bottomCenter,
+                      child: Container(
+                        width: double.infinity,
+                          height: 70,
+                          color: Colors.blue,
+                          child: Center(child: Text('Don\t have an Account SignUp', style: TextStyle(color: Colors.white, fontSize: 20),))),
+                    )
+                )
+              ],
+            ),
           ),
-        ),
-      ),
+        )
     );
   }
 }
